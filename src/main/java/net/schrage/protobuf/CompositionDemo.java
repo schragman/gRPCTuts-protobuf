05b7e3264b27f7ea2993a12f2d8eec1622dd1a99
mschrage <michael.schrage@fincon.eu>
2022-06-23 18:24:26 +0200

    Lists

--- src/main/java/net/schrage/protobuf/CompositionDemo.java
+++ src/main/java/net/schrage/protobuf/CompositionDemo.java
@@ -4,6 +4,8 @@ import net.schrage.models.Address;
 import net.schrage.models.Car;
 import net.schrage.models.Person;
 
+import java.util.ArrayList;
+
 public class CompositionDemo {
 
   public static void main(String[] args) {
@@ -14,16 +16,28 @@ public class CompositionDemo {
         .setCity("Bremen")
         .build();
 
-    Car car = Car.newBuilder()
+    Car passat = Car.newBuilder()
         .setMake("VW")
         .setModel("Passat")
         .setYear(2011)
         .build();
 
+    Car derby = Car.newBuilder()
+        .setMake("VW")
+        .setModel("Derby")
+        .setYear(2003)
+        .build();
+
+    ArrayList<Car> cars = new ArrayList<>();
+    cars.add(passat);
+    cars.add(derby);
+
     Person person = Person.newBuilder()
         .setName("Michael")
         .setAge(53)
-        .setCar(car)
+        .addAllCar(cars)
+        /*.addCar(passat)
+        .addCar(derby)*/
         .setAddress(address)
         .build();
 

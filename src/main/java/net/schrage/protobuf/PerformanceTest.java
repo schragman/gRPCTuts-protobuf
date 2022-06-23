afba60606018d65a56a079e17b12eb2b411bfccf
mschrage <michael.schrage@fincon.eu>
2022-06-23 16:07:28 +0200

    Comparison

---
+++ src/main/java/net/schrage/protobuf/PerformanceTest.java
@@ -0,0 +1,63 @@
+package net.schrage.protobuf;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.protobuf.InvalidProtocolBufferException;
+import net.schrage.json.JPerson;
+import net.schrage.models.Person;
+
+public class PerformanceTest {
+
+  public static void main(String[] args) {
+
+    //json
+    JPerson person = new JPerson();
+    person.setName("sam");
+    person.setAge(10);
+    ObjectMapper mapper = new ObjectMapper();
+
+
+    Runnable json = () -> {
+      try {
+        byte[] bytes =  mapper.writeValueAsBytes(person);
+        JPerson person1 = mapper.readValue(bytes, JPerson.class);
+      } catch (Exception e) {
+        throw new RuntimeException(e);
+      }
+    };
+
+    //protobuf
+    Person sam = Person.newBuilder()
+        .setName("sam")
+        .setAge(10)
+        .build();
+
+    Runnable proto = () -> {
+      try {
+        byte[] bytes2 = sam.toByteArray();
+        Person sam1 = Person.parseFrom(bytes2);
+      } catch (InvalidProtocolBufferException e) {
+        throw new RuntimeException(e);
+      }
+    };
+
+    for (int i = 0; i < 5; i++) {
+      runPerformanceTest(proto, "PROTO");
+      runPerformanceTest(json, "JSON");
+    }
+
+  }
+
+  public static void runPerformanceTest(Runnable runnable, String method) {
+
+    long time1 = System.currentTimeMillis();
+    for (int i = 0; i < 5_000_000; i++) {
+      runnable.run();
+    }
+    long time2 = System.currentTimeMillis();
+
+    System.out.println(method + ":" + (time2 - time1) + " ms");
+
+  }
+
+}

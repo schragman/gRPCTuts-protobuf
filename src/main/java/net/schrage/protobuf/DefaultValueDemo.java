cdf623509594654f1f33242beecb21043fd58daf
mschrage <michael.schrage@fincon.eu>
2022-06-23 20:59:33 +0200

    Aufspalten in mehrere Dateien und Packages

--- src/main/java/net/schrage/protobuf/DefaultValueDemo.java
+++ src/main/java/net/schrage/protobuf/DefaultValueDemo.java
@@ -1,4 +1,11 @@
 package net.schrage.protobuf;
 
+import net.schrage.models.Person;
+
 public class DefaultValueDemo {
+
+  public static void main(String[] args) {
+
+    Person person = Person.newBuilder().build();
+  }
 }
